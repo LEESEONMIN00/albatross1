7e570d091806e9a24b19a4dafbaf8ed24015983e
LEESEONMIN00 <leesm.tomato@gmail.com>
2022-06-07 16:01:26 +0900

    ALBATROSS HELMET

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <>
     <Helmet>
-      <title>ALBATROSS</title>
+      <title>ALBATROSS FORK</title>
     </Helmet>
       <Routes>
         <Route path="/login" element={<Login />} />
